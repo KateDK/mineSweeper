7782312ba592121dea73cfd71eaa36f54d040bb0
Kate Dubitski <kate.dubitski@gmail.com>
2018-12-03 10:37:58 -0500

    creating grid in the store and passing it to game component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { initiateGame, dificulties } from '../store';
 
 class Game extends Component {
+  componentDidMount() {
+    this.props.initiateGame(dificulties.easy);
+  }
   render() {
     return <h1>IM THE GAME!!!</h1>;
   }
 }
 
+const mapDispatch = dispatch => {
+  return {
+    initiateGame: dificulty => dispatch(initiateGame(dificulty)),
+  };
+};
 export default connect(
   null,
-  null
+  mapDispatch
 )(Game);
--- src/store/game.js
+++ src/store/game.js
@@ -1,3 +1,32 @@
+const easy = {
+  rows: 10,
+  cols: 10,
+  mines: 5,
+};
+
+const medium = {
+  rows: 20,
+  cols: 20,
+  mines: 10,
+};
+
+const hard = {
+  rows: 30,
+  cols: 30,
+  mines: 15,
+};
+
+const cell = {
+  isMine: false,
+  isOpen: false,
+  closeMines: 0,
+};
+export const dificulties = {
+  easy,
+  medium,
+  hard,
+};
+
 /**
  * ACTION TYPES
  */
@@ -7,24 +36,39 @@ const INITIATE_GAME = 'INITIATE_GAME';
 /**
  * INITIAL STATE
  */
-const initialState = 'Hello from the Redux Store';
+const initialState = {
+  grid: [],
+  gameWon: false,
+  gameOver: false,
+  openCells: 0,
+  dificulty: easy,
+};
 
 /**
  * ACTION CREATORS
  */
 export const getState = () => ({ type: INITIATE_GAME, toAdd: '!' });
-
-/**
- * THUNK CREATORS
- */
+export const initiateGame = dificulty => ({ type: INITIATE_GAME, dificulty });
 
 /**
  * REDUCER
  */
 export default function(state = initialState, action) {
   switch (action.type) {
-    case INITIATE_GAME:
-      return state + action.toAdd;
+    case INITIATE_GAME: {
+      let game = { ...initialState, dificulty: action.dificulty };
+      const rows = game.dificulty.rows;
+      const cols = game.dificulty.cols;
+
+      for (let row = 0; row < rows; row++) {
+        let currentRow = [];
+        for (let col = 0; col < cols; col++) {
+          currentRow.push({ ...cell });
+        }
+        game.grid.push(currentRow);
+      }
+      return game;
+    }
     default:
       return state;
   }
